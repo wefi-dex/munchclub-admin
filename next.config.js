7115d896cef90774b1a34fff269f741aae9a40fa
wefi-dex <www.walter1125@gmail.com>
2025-10-03 03:43:03 +0900

    fix external ip issue

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
     images: {
         domains: ['localhost', 'themunchclub.com', 'res.cloudinary.com', 'images.unsplash.com'],
     },
+    allowedDevOrigins: [
+        '13.53.44.1',
+        'localhost',
+        '127.0.0.1'
+    ],
 }
 
 module.exports = nextConfig
